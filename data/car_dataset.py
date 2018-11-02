e8a2aeadae35870653a6261adb31c79ca362e3c0
Jannik Zürn <jannik.zuern@gmail.com>
2018-11-02 09:26:57 +0100

    Update and rename cifar_dataset.py to car_dataset.py

--- data/car_dataset.py
+++ data/car_dataset.py
@@ -7,8 +7,7 @@ import numpy as np
 from model.hyper_parameters import params
 from random import shuffle
 import scipy.io
-
-
+import string
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
@@ -18,13 +17,34 @@ def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
+def _path_to_features(writer, label, image_path):
+
+    print("feature label = ", label)
+
+    pil_img = Image.open(image_path)  # open image and convert to grayscale
+    res = params["architecture"]["image_resolution"]
+
+    pil_img = pil_img.resize([res, res])
+
+    img = np.asarray(pil_img, dtype=np.uint8)
+    img = img.astype(np.float32)
+    img = np.multiply(img, 1.0 / 255.0)
+
+    # Create a feature
+    feature = {'label': _int64_feature(label),
+               'input_1': _bytes_feature(img.tostring())}
+
+    sample = tf.train.Example(features=tf.train.Features(feature=feature))
+    writer.write(sample.SerializeToString())
+
+
+
 def maybe_convert_to_tfrecords():
 
     root = params["paths"]["data_dir"]
 
     train_filename = root + 'train.tfrecords'
     eval_filename = root + 'eval.tfrecords'
-    mat_file = root + 'cars_annos.mat'
 
     # Test if tfrecords files already exist
     if os.path.isfile(train_filename) and os.path.isfile(eval_filename):
@@ -32,150 +52,68 @@ def maybe_convert_to_tfrecords():
         print("Training and Evaluation tfrecord files already exist!")
         return
 
-    image_paths = []
-    gt_labels = []
+    # get names for classes
+    class_names = []
 
-    # Training data
-    train_writer = tf.python_io.TFRecordWriter(train_filename)
+    with open(root + '/names.csv') as csvDataFile:
+        csv_reader = csv.reader(csvDataFile, delimiter=';')
+        for row in csv_reader:
+            class_names.append(row[0])
 
-    mat =  scipy.io.loadmat(mat_file)
 
-    print(mat)
-
-    # Validation data
-    with open(csv_file, 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        for row in reader:
-
-            gt_labels.append(row[0])
-
-            image_id = row[1]
-            image_path = root + 'images/' + image_id + '.jpg'
-
-            image_paths.append(image_path)
-
+    train_data_dir = root + 'car_data/train/'
 
     # open the TFRecords file
+    train_writer = tf.python_io.TFRecordWriter(train_filename)
+
+    train_counter = 0
+
+    with open(root + '/anno_train.csv') as csvDataFile:
+
+        csvReader = csv.reader(csvDataFile, delimiter=',')
+
+        for row in csvReader:
+
+            if train_counter < 100:
+
+                print(row)
+                label = int(row[5]) - 1  # compensate for class index offset of 1
+
+                class_path = class_names[label]
+                class_path = string.replace(class_path, '/', '-')
+
+                image_path = train_data_dir + class_path + '/' + row[0]
+
+                _path_to_features(train_writer, label, image_path)
+
+                train_counter += 1
+
     eval_writer = tf.python_io.TFRecordWriter(eval_filename)
 
-    n_samples = len(image_paths)
-    images = []
-    labels = []
+    validation_data_dir = root + 'car_data/test/'
+    eval_counter = 0
 
-    all_class_counter = [0]*params["architecture"]["n_output_classes"]
+    with open(root + '/anno_test.csv') as csvDataFile:
 
+        csvReader = csv.reader(csvDataFile, delimiter=',')
+        for row in csvReader:
 
-    for i in range(n_samples):
+            if eval_counter < 100:
 
-        if not i % 100:
-            print('Serializing sample {}/{}'.format(i, n_samples))
-            sys.stdout.flush()
+                print(row)
+                label = int(row[5]) - 1  # compensate for class index offset of 1
 
-        # Load the image
-        try:
-            pil_img = Image.open(image_paths[i])
-        except:
-            continue
+                class_path = class_names[label]
+                class_path = string.replace(class_path, '/', '-')
 
-        # get label of current sample
-        label = label_dict[dxs[i]]
+                image_path = validation_data_dir + class_path + '/' + row[0]
 
-        # only allow 100 samples per class
-        if all_class_counter[label] > 100:
-            continue
-        
-        all_class_counter[label] += 1
+                _path_to_features(eval_writer, label, image_path)
 
-        # image resolution: 600 x 450
-        w = 600
-        h = 450
-
-        res = params["architecture"]["image_resolution"]
-
-        pil_img = pil_img.crop((75, 0, w-75, h))
-        pil_img = pil_img.resize([res, res])
-
-        img = np.asarray(pil_img, dtype=np.uint8)
-        img = img.astype(np.float32)
-        img = np.multiply(img, 1.0 / 255.0)
-
-        labels.append(label)
-        images.append(img)
-
-    combined = list(zip(images, labels))
-
-    # random shuffle images and labels
-    shuffle(combined)
-
-    # separate
-    images, labels = zip(*combined)
-
-    # split into train, test, and validation set
-    training_fraction = 0.8
-    training_size = int(round(training_fraction*len(images)))
-
-    # training
-    train_images = images[:training_size] 
-    train_labels = labels[:training_size] 
-
-    # evaluation
-    validation_images = images[training_size:] 
-    validation_labels = labels[training_size:] 
-
-    assert(len(validation_images) == len(validation_labels))
-    assert(len(train_images) == len(train_labels))
-
-    len_training_examples = len(train_labels)
-    len_validation_examples = len(validation_labels)
-
-    print("Training set has {} samples.".format(len_training_examples))
-    print("Validation set has {} samples.".format(len_validation_examples))
-
-    # create bins for determining the class distribution in the datasets
-    training_class_counter = [0]*params["architecture"]["n_output_classes"]
-    val_class_counter = [0]*params["architecture"]["n_output_classes"]
-
-    
-    for image, label in zip(validation_images, validation_labels):
-
-        #if label == 1:
-        #    continue
-
-        val_class_counter[label] += 1
-
-        # Create a feature
-        feature = {'label': _int64_feature(label),
-                   'image': _bytes_feature(image.tostring())}
-
-        sample = tf.train.Example(features=tf.train.Features(feature=feature))
-
-        eval_writer.write(sample.SerializeToString())
-
-    for image, label in zip(train_images, train_labels):
-
-        #if label == 1:
-        #    continue
-
-        training_class_counter[label] += 1
-
-        # Create a feature
-        feature = {'label': _int64_feature(label),
-                   'image': _bytes_feature(image.tostring())}
-
-        sample = tf.train.Example(features=tf.train.Features(feature=feature))
-
-        train_writer.write(sample.SerializeToString())
-
+                eval_counter += 1
 
     train_writer.close()
     eval_writer.close()
-
-    print("Distribution of classes in Training set:")
-    print(training_class_counter)
-
-    print("Distribution of classes in Evaluation set:")
-    print(val_class_counter)
-
 
     sys.stdout.flush()
 
@@ -187,7 +125,7 @@ def parse_fn(serialized):
 
     features = \
         {
-            'image': tf.FixedLenFeature([], tf.string),
+            'input_1': tf.FixedLenFeature([], tf.string),
             'label': tf.FixedLenFeature([], tf.int64)
         }
 
@@ -196,7 +134,7 @@ def parse_fn(serialized):
                                              features=features)
 
     # Get the image as raw bytes.
-    image_raw = parsed_example['image']
+    image_raw = parsed_example['input_1']
 
     # Decode the raw bytes so it becomes a tensor with type.
     image = tf.decode_raw(image_raw, tf.float32)
@@ -205,7 +143,6 @@ def parse_fn(serialized):
     res = params["architecture"]["image_resolution"]
     
     image = tf.reshape(image, [res, res, 3])
-
 
     # Get the label associated with the image.
     label = parsed_example['label']
@@ -263,13 +200,13 @@ def train_input_fn(batch_size=params["training"]["train_batch_size"], buffer_siz
     images_batch, labels_batch = iterator.get_next()
 
     # The input-function must return a dict wrapping the images.
-    print("eval_input_fn: images_batch = ", images_batch)
+    print("train_input_fn: images_batch = ", images_batch)
+    res = params["architecture"]["image_resolution"]
 
-    images_batch = tf.reshape(images_batch, [1, 16, 16, 3])
-    print("eval_input_fn: x = ", images_batch)
+    images_batch = tf.reshape(images_batch, [-1, res, res, 3])
+    print("train_input_fn: x = ", images_batch)
 
-
-    x = {'image': images_batch}
+    x = {'input_1': images_batch}
     y = labels_batch
 
     return x, y
@@ -320,11 +257,12 @@ def eval_input_fn(batch_size=params["training"]["validation_batch_size"], buffer
     images_batch, labels_batch = iterator.get_next()
 
     # The input-function must return a dict wrapping the images.
+    res = params["architecture"]["image_resolution"]
 
-    images_batch = tf.reshape(images_batch, [1, 32, 32, 3])
+    images_batch = tf.reshape(images_batch, [-1, res, res, 3])
     print("eval_input_fn: x = ", images_batch)
 
-    x = {'image': images_batch}
+    x = {'input_1': images_batch}
     y = labels_batch
 
 
@@ -379,11 +317,12 @@ def predict_input_fn(batch_size=params["training"]["test_batch_size"], buffer_si
     images_batch, labels_batch = iterator.get_next()
 
     # The input-function must return a dict wrapping the images.
+    res = params["architecture"]["image_resolution"]
 
-    images_batch = tf.reshape(images_batch, [1, 32, 32, 3])
+    images_batch = tf.reshape(images_batch, [-1, res, res, 1])
     print("eval_input_fn: x = ", images_batch)
 
-    x = {'image': images_batch}
+    x = {'input_1': images_batch}
     y = labels_batch
 
     return x, y
